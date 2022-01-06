f023c0bdf039452b68bb3862d95da0bbe4a9c408
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2022-01-06 15:49:55 +0100

    front and back

---
+++ client/src/context/appContext.js
@@ -0,0 +1,41 @@
+import React, { useReducer, useContext } from 'react';
+import reducer from './reducer';
+import { CLEAR_ALERT, DISPLAY_ALERT } from './action';
+
+const inititalState = {
+  isLoading: false,
+  showAlert: false,
+  alertText: '',
+  alertType: '',
+};
+
+const AppContext = React.createContext();
+
+const AppProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, inititalState);
+
+  const displayAlert = () => {
+    dispatch({ type: DISPLAY_ALERT });
+    clearAlert();
+  };
+
+  const clearAlert = () => {
+    setTimeout(() => {
+      dispatch({
+        type: CLEAR_ALERT,
+      });
+    }, 3000);
+  };
+
+  return (
+    <AppContext.Provider value={{ ...state, displayAlert }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+const useAppContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppProvider, inititalState, useAppContext };
