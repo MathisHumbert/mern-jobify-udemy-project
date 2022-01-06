d4b4093232f4a1ce3d10140d74ba251d0f625db9
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2022-01-06 18:19:42 +0100

    working on back end

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,9 +1,28 @@
-const register = async (req, res) => {
-  res.send('register user');
+import User from '../models/User.js';
+import { StatusCodes } from 'http-status-codes';
+import { BadRequestError } from '../errors/index.js';
+
+const register = async (req, res, next) => {
+  // handle error not with mongoose required
+  const { name, email, password } = req.body;
+  if (!name || !email || !password) {
+    throw new BadRequestError('please provide all values');
+  }
+
+  // handle error not with mongoose unique
+  const userAlreadyExists = await User.findOne({ email });
+  if (userAlreadyExists) {
+    throw new BadRequestError('Email already in use');
+  }
+
+  const user = await User.create(req.body);
+  res.status(StatusCodes.CREATED).json({ user });
 };
+
 const login = async (req, res) => {
   res.send('login user');
 };
+
 const updateUser = async (req, res) => {
   res.send('update user');
 };
