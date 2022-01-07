e2e657879b06180a2c4b4b142a433b68f5330a51
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2022-01-07 13:19:39 +0100

    pages structure is done

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,33 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Landing, Dashboard, Register, Error } from './pages';
+import { Landing, Register, Error, ProtectedRoute } from './pages';
+import {
+  AddJob,
+  AllJobs,
+  Profile,
+  SharedLayout,
+  Stats,
+} from './pages/dashboard';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Dashboard />} />
+        {/* nested pages */}
+        <Route
+          path='/'
+          element={
+            // protected route
+            <ProtectedRoute>
+              <SharedLayout />
+            </ProtectedRoute>
+          }
+        >
+          {/* index === relaive path / homepage */}
+          <Route index element={<Stats />} />
+          <Route path='all-jobs' element={<AllJobs />} />
+          <Route path='add-job' element={<AddJob />} />
+          <Route path='profile' element={<Profile />} />
+        </Route>
         <Route path='/register' element={<Register />} />
         <Route path='/landing' element={<Landing />} />
         <Route path='*' element={<Error />} />
---
+++ client/src/pages/ProtectedRoute.js
@@ -0,0 +1,15 @@
+import { useAppContext } from '../context/appContext';
+import { Navigate } from 'react-router-dom';
+
+const ProtectedRoute = ({ children }) => {
+  const { user } = useAppContext();
+  // if there is no user loged in we redirect to the landing page
+  if (!user) {
+    return <Navigate to='/landing' />;
+  }
+  // if there is one we allow him to access pages
+
+  return children;
+};
+
+export default ProtectedRoute;
