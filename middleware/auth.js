856e4f84af571e3883a211c42a9b2f82937f227d
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2022-01-11 09:06:45 +0100

    popualte js is done

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,7 +3,6 @@ import { UnAuthenticatedError } from '../errors/index.js';
 
 const auth = async (req, res, next) => {
   const authHeaders = req.headers.authorization;
-  console.log(authHeaders);
   if (!authHeaders || !authHeaders.startsWith('Bearer')) {
     throw new UnAuthenticatedError('Authentication Invalid');
   }
